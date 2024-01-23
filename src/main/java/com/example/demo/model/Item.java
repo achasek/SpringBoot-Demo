9acff5522f43e57631261c72258c360896157ff9
achasek <chaseknieper@gmail.com>
2024-01-23 11:55:11 -0700

    anutha commit

---
+++ src/main/java/com/example/demo/model/Item.java
@@ -0,0 +1,15 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Item {
+
+}
+
+//name: DataTypes.STRING,
+//price: DataTypes.INTEGER,
+//description: DataTypes.STRING,
+//category: DataTypes.STRING,
+//image: DataTypes.STRING
+//id maybe?: number
